75ab2293bcc885dcb8e4e4a06c61f72ded667d0c
stlethom <leon.thomanek@stud.h-da.de>
2021-08-24 14:13:43 +0200

    add both random

--- backend/website/request.py
+++ backend/website/request.py
@@ -8,11 +8,18 @@ from . import db
 request = Blueprint('request', __name__)
 
 
-@request.route('/randomSchaetzen', methods=['GET', 'POST'])
-def home():
+@request.route('/randomSchaetzen', methods=['GET'])
+def randomSchaetzen():
     # result = db.session.execute(select(schaetzen).order_by(schaetzen.id))   
     result = db.session.query(schaetzen).order_by(func.random()).first()
     print('asdasd sad asd asd asd a' ,result) 
     return jsonify({'result':result})
 
+
+
+@request.route('/randomTwo', methods=['GET'])
+def randomTwo():
+    result = db.session.query(two_idiots).order_by(func.random()).first()
+    print('asdasd sad asd asd asd a' ,result) 
+    return jsonify({'result':result})
 
