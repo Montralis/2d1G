d086985ccb9f77b4f000047f2fff25312f9f9f56
stlethom <leon.thomanek@stud.h-da.de>
2021-09-13 18:40:45 +0200

    cahnge route to index page

--- backend/website/__init__.py
+++ backend/website/__init__.py
@@ -6,14 +6,17 @@ from dotenv import load_dotenv
 def create_app():
     load_dotenv()
 
-    app = Flask(__name__)
+    template_dir = os.path.dirname(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+    template_dir = os.path.join(template_dir, 'frontend')
+
+    app = Flask(__name__, template_folder = template_dir)
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 
-    # from .views import views
+    from .views import views
     from .request import request
 
-    # app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(request, url_prefix='/')
 
 
--- backend/website/views.py
+++ backend/website/views.py
@@ -1,47 +1,10 @@
 from flask import Blueprint, render_template, request, flash
-from faker import Faker
-from .models import schaetzen, two_idiots
-from . import db
+
 
 views = Blueprint('views', __name__)
 
 
 @views.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template("home.html")
+    return render_template("index.html")
 
-
-
-@views.route('/addguess', methods=['GET', 'POST'])
-def addguess():
-    if request.method == 'POST':
-        antwort = request.form.get('antwort')
-        frage = request.form.get('frage')
-
-        if len(frage)< 1:
-            flash('Keine Frage eingetragen', category='error')
-        elif len(antwort) < 1:
-            flash('Keine Antwort eingetragen', category='error')
-        else:
-            new_schaetzen = schaetzen(frage = frage, antwort = antwort)
-            db.session.add(new_schaetzen)
-            db.session.commit()
-            flash('Frage added!', category='success')
-
-    return render_template("addguess.html")
-
-@views.route('/addtwoiditos', methods=['GET', 'POST'])
-def addtwoiditos():
-    if request.method == 'POST':
-        categorie = request.form.get('categorie')
-
-        if len(categorie)< 1:
-            flash('Keine Kategorie eingetragen', category='error')
-
-        else:
-            new_two = two_idiots(categorie = categorie)
-            db.session.add(new_two)
-            db.session.commit()
-            flash('Frage added!', category='success')
-
-    return render_template("addtwoidiots.html")
