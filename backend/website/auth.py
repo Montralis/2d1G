433fc65552f399d23abb364de366baeea950fcdc
Montralis <52356382+Montralis@users.noreply.github.com>
2021-09-14 13:49:21 +0200

    delete username

--- backend/website/auth.py
+++ backend/website/auth.py
@@ -10,12 +10,11 @@ hashed_password = 'c4b991b74c38acc0b4d0a145d7d0949c64af3abd3e8d29acd8cd7010ce0bb
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        email = request.form.get('username')
         password = request.form.get('password')
 
         if check_password_hash(hashed_password, password):
             flash('Logged in successfully!', category='success')
-            login_user({'username': username, 'password': hashed_password}, remember=True)
+            login_user({'password': hashed_password}, remember=True)
             return redirect(url_for('views.adddata'))
         else:
             flash('Incorrect password, try again.', category='error')
