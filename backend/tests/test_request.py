9f02563138ac17f91971cd13980ad3a54492f339
Montralis <52356382+Montralis@users.noreply.github.com>
2021-09-16 13:50:21 +0200

    Create test_request.py

---
+++ backend/tests/test_request.py
@@ -0,0 +1,8 @@
+def test_new_user():
+    """
+    GIVEN a User model
+    WHEN a new User is created
+    THEN check the email, hashed_password, and role fields are defined correctly
+    """
+
+    assert 1 == 1
