47b1f1b84d6690683edd1b4ff52c25419ad0a432
Leon <Leon.thomanek@stud.h-da.de>
2021-09-18 22:40:13 +0200

    add serverconfig file

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,17 @@
 import os
 from website import create_app
+import serverconf as cfg
 
 app = create_app()
 
 if __name__ == '__main__':
-   from waitress import serve
-   serve(app, host="0.0.0.0", port=5000)
+
+   if cfg.server['modus'] == 'dev':
+      app.run(debug = True, port = cfg.server['port'])
+
+   elif cfg.server['modus'] == 'prod':
+      from waitress import serve
+      serve(app, host = cfg.server['ip'], port=cfg.server['port'])
+   
+   else:
+      print('unknown server modus, check serverconf file')
---
+++ backend/serverconf.py
@@ -0,0 +1,11 @@
+# configuration file for the server
+
+# server setting
+# set modus=dev for development and modus=prod for production
+server = {
+    "modus": "dev",
+    "ip": "0.0.0.0",
+    "port": "5000",
+}
+
+use_anonymous = True
