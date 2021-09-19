7e353058fd41cb3fa39f06f3e0cecf184a11f72d
Leon <Leon.thomanek@stud.h-da.de>
2021-09-19 09:53:01 +0200

    setup server config

--- backend/serverconf.py
+++ backend/serverconf.py
@@ -8,4 +8,16 @@ server = {
     "port": "5000",
 }
 
+dev_files = {
+    "guess": "/website/data/guess.json",
+    "two-idiots": "/website/data/two-idiots.json",
+    "different-word": "/website/data/guess.json",
+}
+
+prod_files = {
+    "guess": "/app/backend/website/data/guess.json",
+    "two-idiots": "/app/backend//website/data/two-idiots.json",
+    "different-word": "/app/backend//website/data/guess.json",
+}
+
 use_anonymous = True
